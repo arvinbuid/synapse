bd5ac3bb5c27bd3c006c71a6702f54c4a6dd8534
Arvin Buid <arvinbuid01@gmail.com>
2025-03-26 17:11:21 +0800

    Add clerk authentication and update favicon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,13 @@
 import type {Metadata} from "next";
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
@@ -23,8 +32,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <header className='flex justify-end items-center p-4 gap-4 h-16'>
+            <SignedOut>
+              <SignInButton mode='modal' />
+              <SignUpButton mode='modal' />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
