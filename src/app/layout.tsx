7dc17c5c0742d0d3eb26f883cee080244b117ae8
Arvin Buid <arvinbuid01@gmail.com>
2025-03-27 11:18:15 +0800

    Creating navbar, desktop and mobile navbar components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import {ClerkProvider} from "@clerk/nextjs";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {ThemeProvider} from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -27,7 +28,19 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <div className='min-h-screen'>
+              <Navbar />
+
+              <main className='py-8'>
+                {/* Container to center the content */}
+                <div className='max-w-7xl mx-auto px-4'>
+                  <div className='grid grid-cols-1 lg:grid-cols-12 gap-6'>
+                    <div className='hidden lg:block col-span-3'>Sidebar</div>
+                    <div className='lg:col-span-9'>{children}</div>
+                  </div>
+                </div>
+              </main>
+            </div>
           </ThemeProvider>
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import {ModeToggle} from "@/components/ModeToggle";
-import {Button} from "@/components/ui/button";
-import {SignInButton, SignUpButton, SignedIn, SignedOut, UserButton} from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className='m-4 flex gap-3'>
-      <SignedOut>
-        <SignInButton mode='modal'>
-          <Button>Sign In</Button>
-        </SignInButton>
-        <SignUpButton mode='modal'>
-          <Button variant='secondary'>Sign Up</Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
+      <h1>Homepage</h1>
     </div>
   );
 }
